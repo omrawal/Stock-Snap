a86b2b803a702e846be06283c27301d5e2cb8872
omrawal <omrawal007@gmail.com>
2024-07-31 21:56:59 +0530

    Code restructuring

--- util/google_finance_extraction.py
+++ util/google_finance_extraction.py
@@ -30,20 +30,34 @@ print(previous_close_value)
 print(previous_close_value.text)
 
 
+def get_currency_and_symbol(value_string)->tuple:
+    currency_symbol = value_string[0]
+    if currency_symbol in CURRENCY_SYMBOLS:
+        return (CURRENCY_SYMBOLS.get(currency_symbol,None),currency_symbol)
+    else:
+        return (None,None)
 
-def get_lpt(soup,quote_lpt_class="YMlKec fxKbKc"):
+def get_ltp_string(soup,quote_lpt_class="YMlKec fxKbKc") -> str:
     ltp_content = soup.find(class_=quote_lpt_class)
     if ltp_content is not None:
         quote_ltp = ""
-        ltp_content = soup.find(class_=quote_lpt_class)
         quote_ltp = ltp_content.text.replace(",", "")
         currency_symbol = quote_ltp[0]
-        print(currency_symbol)
+        # print(currency_symbol)
         return quote_ltp
     else:
         print("LTP content is None")
         return None
 
+def get_previous_close_string(soup,prev_close_class="gyFHrc"):
+    previous_close_content = soup.find_all(class_=prev_close_class)[0]
+    if previous_close_content is not None:
+        previous_close_tag = BeautifulSoup(str(previous_close_content), "html.parser")
+        previous_close_value = previous_close_tag.find(class_="P6K39c").text.replace(",", "")
+        return previous_close_value
+    else:
+        print("Previous close content is None")
+        return None
 
 
 def get_quotes_of_all_500_nse_companies():
@@ -56,7 +70,7 @@ def get_quotes_of_all_500_nse_companies():
         google_quote_fetch_url = google_quote_base_url + f"{company_symbol}:{exchange_symbol}"
         response = requests.get(google_quote_fetch_url)
         soup = BeautifulSoup(response.text, "html.parser")
-        print("Get ltp is ->",get_lpt(soup))
+        print("Get ltp is ->",get_ltp_string(soup))
         quote_ltp = ""
         desc = ""
         currency = ""
@@ -68,20 +82,17 @@ def get_quotes_of_all_500_nse_companies():
         ltp_content = soup.find(class_=quote_lpt_class)
         desc_content = soup.find(class_=description_class)
         if ltp_content is not None:
-            quote_ltp = ltp_content.text.replace(",", "")
-            currency_symbol = quote_ltp[0]
-
-            previous_close_content = soup.find_all(class_=prev_close_class)[0]
-            previous_close_tag = BeautifulSoup(str(previous_close_content), "html.parser")
-            previous_close_value = previous_close_tag.find(class_="P6K39c").text.replace(",", "")
-            if previous_close_value[0] in CURRENCY_SYMBOLS:
-                previous_close_value = previous_close_value[1:]
-
-            if currency_symbol in CURRENCY_SYMBOLS:
-                currency = CURRENCY_SYMBOLS[currency_symbol]
-                quote_ltp = quote_ltp[1:]
-            else:
-                currency = "Unknown"
+            quote_ltp_str_value = get_ltp_string(soup)
+            ltp_currency,ltp_currency_symbol = get_currency_and_symbol(quote_ltp_str_value)
+            print("LTP symbol ",ltp_currency,ltp_currency_symbol)
+            if ltp_currency is not None or ltp_currency_symbol is not None:
+                quote_ltp = quote_ltp_str_value[1:]
+            
+            previous_close_str_value = get_previous_close_string(soup)
+            previous_close_currency,previous_close_currency_symbol = get_currency_and_symbol(previous_close_str_value)
+            print("previous_close symbol ",previous_close_currency,previous_close_currency_symbol)
+            if previous_close_currency is not None or previous_close_currency_symbol is not None:
+                previous_close_value = previous_close_str_value[1:]
 
             change_amount = float(quote_ltp) - float(previous_close_value)
 
@@ -96,8 +107,8 @@ def get_quotes_of_all_500_nse_companies():
 
         # print("change_amount", change_amount)
         # print(quote_ltp)
-        print({company_symbol: {'ltp': quote_ltp, 'desc': desc,
-                                "currency": currency, "percentage_change": str(percentage_change) + str("%"),
+        print({company_symbol: {'ltp': quote_ltp, 'desc': desc,"previous_close": previous_close_value,
+                                "currency": ltp_currency, "percentage_change": str(percentage_change) + str("%"),
                                 "change_amount": change_amount, "currency_symbol": currency_symbol,
                                 "change_type": change_type}})
         # except Exception as e:
